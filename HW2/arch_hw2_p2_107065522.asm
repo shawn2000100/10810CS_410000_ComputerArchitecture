e1b93be91fc8f63e953ce0e03de69f31cdb37259
shawn2000100 <shawn2000100@gmail.com>
2019-10-04 19:03:11 +0800

    1004 finished

--- HW2/arch_hw2_p2_107065522.asm
+++ HW2/arch_hw2_p2_107065522.asm
@@ -19,80 +19,84 @@ _Start:
   	add $t0, $zero, $v0 # $t0 = scanf("%d", &v0)
   	
   	# For Debug ----------------------------------------------
-  	li $v0, 1	    # print integer
-  	add $a0, $zero, $t0  # $a0 = input 
-	syscall	            # print integer at $a0	
-	jal  PrintNewline    # For printing new line
+  	#add $a0, $zero, $t0  # $a0 = input 
+  	#li $v0, 1	    # print integer
+	#syscall	            # print integer at $a0	
+	#jal  PrintNewline    # For printing new line
 	# ----------------------------------------------------------
   	
   	#if input = 0, goto _Exit
   	beq $t0, $zero, _Exit
   	
-  	# If $t0 < 0 || $t0 > 10, goto _OutofRange
+  	# If $t0 < 0 || $t0 > 10, goto _Start
+  	# t0 < 0
+  	slt  $t1, $t0, $zero
+  	#  t0 > 10  (10 < t0)
+  	addi $t2, $zero, 10
+  	slt  $t3, $t2, $t0
+  	or  $t1, $t1, $t3 # $t0 < 0 || $t0 > 10
+  	# Debug ------------
+  	#add $a0, $zero, $t1
+  	#li $v0, 1	   # ready to print string
+	#syscall	           # print
+  	# --------------------
+  	addi $t2, $zero, 1
+  	beq  $t1, $t2, _Start
   	
-  	
-  	
-  	
-  	
-  	
-  	
-  	#If input =7, compute input * 2, and print out the answer.
+  	#If $t0  == 7, compute input * 2.
+	addi $t1, $zero, 7
+	bne  $t0, $t1, _function_others
+	sll  $t1, $t0, 1
 	
-	
-	            
-	
-	
-  	
-  	
-  	 
-		
-		
+	#  print out the answer 14
+	la $a0, str2         # print "A * 2 = "
+	li $v0, 4	   # ready to print string
+	syscall	            # print string at $a0	
+  	add $a0, $zero, $t1  # $a0 = input 
+	li $v0, 1	    # print integer
+	syscall	            # print integer at $a0	
+	jal  PrintNewline    # For printing new line         
 	
   	#After print out, go back to wait the user to enter number.
   	j _Start
   	
 _function_others:
 	#setup counter i
+	addi $t1, $zero, 0 # i = 0
 	
-	 
 	#Write a Loop for print ******
 	Loop:
-	
-	#Chech the input and the counter
-	
-	
+	#Check the input and the counter
+	slt $t8, $t1, $t0 # $t8 = $t1 < $t0
 	#End the loop and wait the user to enter number.
-	
+	beq $t8, $zero, _Start
 
-	# print out ******
-	
-	
-	 
+	# print out ******  (if t1 < t0)
+	la $a0, str3      # load address of string to print
+	li $v0, 4	 # ready to print string
+	syscall	         # print
 	
 	#print out the counter number
-	             
-	 
-	
-	
+	add $a0, $zero, $t1     # load address of string to print
+	li $v0, 1	 # ready to print string
+	syscall	         # print
+	                          
 	# print out ******
+	la $a0, str3      # load address of string to print
+	li $v0, 4	 # ready to print string
+	syscall	         # print
 	             
-	
-	 
-	
 	# print out \n (´«¦æ)
-	
+	jal  PrintNewline    # For printing new line         
 		
-		
-	
 	#counter ++
-	
-	
+	addi $t1, $t1, 1
 	#go back to the Loop
 	j Loop                
 
 
 PrintNewline:
-la $a0, Pnewline  # load address of string to print
+la $a0, Pnewline # load address of string to print
 li $v0, 4	 # ready to print string
 syscall	         # print
 jr $ra           # return
@@ -100,6 +104,9 @@ jr $ra           # return
 
 #Stop the program
 _Exit:
+        la $a0, str4      # load address of string to print
+	li $v0, 4	  # ready to print string
+	syscall	          # print
 	li $v0, 10
 	syscall
 	
